266d25eae28ee5c28cd0193a23e5a1658e7bfabb
ocelotdiamond <jakepennyroyal19@gmail.com>
2025-03-15 17:49:31 -0400

    Finished Game

--- game/libs.js
+++ game/libs.js
@@ -3,6 +3,12 @@ const ctx = canvas.getContext('2d');
 
 let displayMultiplier;
 
+const mouse = {
+    x: 0,
+    y: 0,
+    down: false
+};
+
 let keys = {};
 
 function keyDown(event) {
@@ -17,12 +23,30 @@ function keyUp(event) {
 
 function onBlur(event) {
     keys = {};
+    mouse.down = false;
+}
+
+function mouseMove(event) {
+    mouse.x = event.offsetX / displayMultiplier;
+    mouse.y = event.offsetY / displayMultiplier;
+}
+
+function mouseUp(event) {
+    mouse.down = false;
+}
+
+function mouseDown(event) {
+    mouse.down = true;
 }
 
 window.onkeydown = keyDown;
 window.onkeyup = keyUp;
 
 window.onblur = onBlur;
+
+canvas.onmousedown = mouseDown;
+canvas.onmouseup = mouseUp;
+canvas.onmousemove = mouseMove;
 
 window.onresize = () => {
     displayMultiplier = Math.floor(Math.min(window.innerWidth, window.innerHeight) / 128);
@@ -32,3 +56,47 @@ window.onresize = () => {
 }
 
 window.onresize();
+
+function drawRect(ctx, x, y, w, h) {
+    ctx.fillRect(x*displayMultiplier, y*displayMultiplier,
+        w*displayMultiplier, h*displayMultiplier);
+}
+
+function drawPoly(ctx, offX, offY, points) {
+    ctx.beginPath();
+    ctx.moveTo(
+        (points[0][0] + offX)*displayMultiplier,
+        (points[0][1] + offY)*displayMultiplier);
+    for (let i = 1; i < points.length; i++) {
+        ctx.lineTo(
+            (points[i][0]+offX)*displayMultiplier,
+            (points[i][1]+offY)*displayMultiplier);
+    }
+    ctx.closePath();
+    ctx.fill();
+}
+
+function drawCircle(ctx, offx, offy, radius) {
+    ctx.beginPath();
+    ctx.arc(
+        offx * displayMultiplier,
+        offy * displayMultiplier,
+        radius * displayMultiplier, 
+        0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function circleCheck(x1, y1, r1, x2, y2, r2) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    const radiusSum = r1 + r2;
+    
+    return dx * dx + dy * dy <= radiusSum * radiusSum;
+}
+
+function drawText(ctx, x, y, str, size, bold = false) {
+    ctx.font = `${bold?'bold ':''}${size * displayMultiplier}px 'Trebuchet MS'`;
+    ctx.textAlign = 'center';
+    ctx.fillText(str, x * displayMultiplier, y * displayMultiplier);
+}
---
+++ game/main.js
@@ -0,0 +1,394 @@
+const stars = [];
+
+for (let i = 0; i < 30; i++) {
+    stars.push({
+        x: 128 * Math.random(),
+        y: 128 * Math.random(),
+        cycle: Math.random() * Math.PI * 2
+    });
+}
+
+let time = 0;
+let start = 0;
+
+const hookPoints = [];
+
+const player = {
+    x: 64,
+    y: 120,
+    dx: 0,
+    dy: 0
+};
+
+for (let i = 0; i < 8; i++) {
+    if (i % 4 === 2) {
+        continue;
+    }
+
+    hookPoints.push({
+        x: 64 + 48 * Math.cos(i * Math.PI * 0.25),
+        y: 64 + 48 * Math.sin(i * Math.PI * 0.25),
+        hovered: false,
+        hoverTime: 0
+    });
+}
+
+let hoverid = -1;
+let enemyHealth = 0;
+let isDead = false;
+
+let enemyBullets = [];
+let playerBullets = [];
+
+const damageParticles = [];
+
+let playerBulletCooldown = 40;
+let enemyBulletCooldown = 30;
+
+function update() {
+    let isAnyHovered = false;
+    for (let i = 0; i < hookPoints.length; i++) {
+        let mouseRadius = 0;
+        if (hookPoints) {
+            mouseRadius = 3;
+        }
+
+        hookPoints[i].hovered = circleCheck(mouse.x, mouse.y, mouseRadius, hookPoints[i].x, hookPoints[i].y, 10);
+
+        if (hookPoints[i].hovered) {
+            hookPoints[i].hoverTime = Math.min(hookPoints[i].hoverTime + 1, 10);
+            hoverid = i;
+            isAnyHovered = true;
+            if (mouse.down) {
+                const xDiff = hookPoints[i].x - player.x;
+                const yDiff = hookPoints[i].y - player.y;
+
+                const distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+                const power = 1 / Math.max(distance, 1);
+
+                player.dx += xDiff * power * 0.05;
+                player.dy += yDiff * power * 0.05;
+            }
+        } else {
+            hookPoints[i].hoverTime = Math.max(hookPoints[i].hoverTime - 1, 0);
+        }
+    }
+
+    if (playerBulletCooldown === 0 && enemyHealth > 0) {
+        playerBulletCooldown = 40;
+        const diffX = mouse.x - player.x;
+        const diffY = mouse.y - player.y;
+
+        const angle = Math.atan2(diffY, diffX);
+        playerBullets.push({
+            x: player.x,
+            y: player.y,
+            dx: Math.cos(angle),
+            dy: Math.sin(angle)
+        });
+    } else {
+        playerBulletCooldown--;
+    }
+
+    if (enemyBulletCooldown === 0 && enemyHealth > 0) {
+        enemyBulletCooldown = 30;
+        const diffX = player.x - 64;
+        const diffY = player.y - 64;
+
+        const angle = Math.atan2(diffY, diffX);
+        enemyBullets.push({
+            x: 64,
+            y: 64,
+            dx: Math.cos(angle),
+            dy: Math.sin(angle)
+        });
+    } else {
+        enemyBulletCooldown--;
+    }
+
+    if (!isAnyHovered) {
+        hoverid = -1;
+    }
+
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].y -= 0.1;
+        if (stars[i].y < -2.1875) {
+            stars[i].y += 132.375;
+            stars[i].x = 128 * Math.random();
+        }
+
+        stars[i].cycle += 0.025;
+    }
+
+    for (let i = 0; i < damageParticles.length; i++) {
+        damageParticles[i].x += damageParticles[i].dx;
+        damageParticles[i].y += damageParticles[i].dy;
+        damageParticles[i].rot += damageParticles[i].dr;
+
+        if (damageParticles[i].y < -10) {
+            damageParticles.splice(i, 1);
+            i--;
+        }
+    }
+
+    for (let i = 0; i < enemyBullets.length; i++) {
+        enemyBullets[i].x += enemyBullets[i].dx;
+        enemyBullets[i].y += enemyBullets[i].dy;
+
+        if (circleCheck(player.x, player.y, 1, enemyBullets[i].x, enemyBullets[i].y, 1)) {
+            isDead = true;
+            return;
+        }
+    }
+
+    for (let i = 0; i < playerBullets.length; i++) {
+        playerBullets[i].x += playerBullets[i].dx;
+        playerBullets[i].y += playerBullets[i].dy;
+
+        if (circleCheck(64, 64, 4, playerBullets[i].x, playerBullets[i].y, 1) && enemyHealth > 0) {
+            playerBullets.splice(i, 1);
+            enemyHealth--;
+            for (let i = 0; i < (enemyHealth === 0 ? 20 : 7); i++) {
+                damageParticles.push({
+                    x: 61 + Math.random() * 6,
+                    y: 60 + Math.random() * 2,
+                    dx: (Math.random() - 0.5) * 0.5,
+                    dy: -1 + Math.random() * 0.5,
+                    rot: 0,
+                    dr: (Math.random() - 0.5) * 0.0625
+                });
+            }
+            i--;
+            continue;
+        }
+    }
+
+    player.x += player.dx;
+    player.y += player.dy;
+
+    player.dx *= 0.99;
+    player.dy *= 0.99;
+
+    if (player.x < -5) {
+        player.x += 138;
+    }
+
+    if (player.y < -5) {
+        player.y += 138;
+    }
+
+    if (player.x > 133) {
+        player.x -= 138;
+    }
+
+    if (player.y > 133) {
+        player.y -= 138;
+    }
+
+    isDead = circleCheck(64, 64, 4, player.x, player.y, 1) && enemyHealth > 0;
+}
+
+function draw() {
+    if (!(keys[' '] && (enemyHealth === 0 || isDead))) {
+        window.requestAnimationFrame(e => draw());
+    } else {
+        startGame();
+        return;
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (enemyHealth === 0) {
+        drawText(ctx, 64, 64, 'You Win!', 10);
+        drawText(ctx, 64, 80, 'Press Space to Restart', 5);
+    } else if (isDead) {
+        ctx.globalAlpha = 0.5;
+        ctx.fillStyle = '#9e1e2f';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        
+        drawText(ctx, 64, 64, 'You Died', 10);
+        drawText(ctx, 64, 80, 'Press Space to Restart', 5);
+
+        document.querySelector(':root').style.setProperty('--bg', '#5f1132');
+        return;
+    }
+
+    if (!isDead) {
+        update();
+    }
+
+    for (let i = 0; i < stars.length; i++) {
+        drawStar(stars[i].x, stars[i].y, stars[i].cycle, true);
+    }
+
+    ctx.fillStyle = '#ffffff';
+    for (let i = 0; i < damageParticles.length; i++) {
+        const poly = [];
+        for (let n = 0; n < 3; n++) {
+            const angle = damageParticles[i].rot + n * Math.PI * 2 / 3;
+            poly.push([Math.cos(angle), Math.sin(angle)]);
+        }
+        drawPoly(ctx, damageParticles[i].x, damageParticles[i].y, poly);
+    }
+
+    drawPlayer(player.x, player.y);
+
+    drawBullets();
+
+    if (enemyHealth > 0) {
+        drawEnemy(64, 64);
+    }
+}
+
+function drawEnemy(x, y) {
+    const enemyPoly = [[0, 3], [-2, -2], [0, -1], [2, -2]];
+
+    for (let i = 0; i < enemyPoly.length; i++) {
+        enemyPoly[i][0] *= 3;
+        enemyPoly[i][1] *= 3;
+    }
+
+    ctx.fillStyle = '#ffffff';
+    drawPoly(ctx, x, y, enemyPoly);
+}
+
+function drawBullets() {
+    for (let i = 0; i < enemyBullets.length; i++) {
+        ctx.fillStyle = '#ff3033';
+        drawCircle(ctx, enemyBullets[i].x, enemyBullets[i].y, 1);
+    }
+
+    for (let i = 0; i < playerBullets.length; i++) {
+        ctx.fillStyle = '#71bde1';
+        drawCircle(ctx, playerBullets[i].x, playerBullets[i].y, 0.5);
+    }
+}
+
+function drawPlayer(x, y) {
+    if (hoverid !== -1) {
+        ctx.setLineDash([]);
+        ctx.beginPath();
+        ctx.moveTo(x * displayMultiplier, y * displayMultiplier);
+        ctx.lineTo(hookPoints[hoverid].x * displayMultiplier, hookPoints[hoverid].y * displayMultiplier);
+        ctx.closePath();
+        ctx.strokeStyle = '#8953b2';
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        ctx.lineWidth = 1;
+    }
+
+    ctx.lineDashOffset = (Date.now() - start) / 100;
+
+    for (let i = 0; i < hookPoints.length; i++) {
+        drawHookPoint(hookPoints[i].x, hookPoints[i].y, hookPoints[i].hoverTime);
+    }
+
+    const diffX = mouse.x - x;
+    const diffY = mouse.y - y;
+    const angle = Math.atan2(diffY, diffX) + Math.PI * 0.5;
+
+    const playerPoly = [[0, -3], [-2, 2], [0, 1], [2, 2]];
+    for (let i = 0; i < playerPoly.length; i++) {
+        const x = playerPoly[i][0];
+        const y = playerPoly[i][1];
+
+        playerPoly[i][0] = x * Math.cos(angle) - y * Math.sin(angle);
+        playerPoly[i][1] = x * Math.sin(angle) + y * Math.cos(angle);
+    }
+    ctx.fillStyle = '#ffffff';
+    drawPoly(ctx, x, y, playerPoly);
+}
+
+function drawStar(x, y, angle, first) {
+    const modifier = first ? 1 : 0.5;
+    const scale = (0.25 + Math.sin(angle) * 0.0625) * modifier;
+    const arr = [[7*scale, 0], [0, 3*scale], [-7*scale, 0], [0, -3*scale]];
+
+    if (first) {
+        ctx.fillStyle = '#411f77';
+    } else {
+        ctx.fillStyle = '#64329a';
+    }
+    drawPoly(ctx, x, y, arr);
+    for (let i = 0; i < 4; i++) {
+        const interchange = arr[i][0];
+        arr[i][0] = arr[i][1];
+        arr[i][1] = interchange;
+    }
+
+    drawPoly(ctx, x, y, arr);
+
+    if (first) {
+        drawStar(x, y, angle, false);
+    }
+}
+
+function drawHookPoint(x, y, hoverTime) {
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = '#120119';
+    drawCircle(ctx, x, y, 10 + hoverTime / 10);
+    drawCircle(ctx, x, y, 7);
+    ctx.globalAlpha = 1;
+
+    if (hoverTime > 0) {
+        ctx.strokeStyle = '#ef86ef';
+        ctx.beginPath();
+        ctx.arc(
+            x * displayMultiplier,
+            y * displayMultiplier,
+            (10 + hoverTime / 10) * displayMultiplier, 
+            0, Math.PI * 2);
+        ctx.closePath();
+        ctx.setLineDash([10, 6, 6, 6]);
+        ctx.stroke();
+    }
+}
+
+function startGame() {
+    document.querySelector(':root').style.setProperty('--bg', '#210435');
+
+    isDead = false;
+    enemyHealth = 20;
+    playerBulletCooldown = 40;
+    enemyBulletCooldown = 30;
+
+    playerBullets = [];
+    enemyBullets = [];
+
+    player.x = 64;
+    player.y = 120;
+    player.dx = 0;
+    player.dy = 0;
+
+    draw();
+}
+
+function titleScreen() {
+    if (!keys[' ']) {
+        window.requestAnimationFrame(e => titleScreen());
+    } else {
+        startGame();
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < stars.length; i++) {
+        drawStar(stars[i].x, stars[i].y, stars[i].cycle, true);
+    }
+
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].y -= 0.1;
+        if (stars[i].y < -2.1875) {
+            stars[i].y += 132.375;
+            stars[i].x = 128 * Math.random();
+        }
+
+        stars[i].cycle += 0.025;
+    }
+
+    ctx.fillStyle = '#edb1ed';
+    drawText(ctx, 64, 64, 'Gravity Swinger', 10);
+    drawText(ctx, 64, 80, 'Press Space to Start', 5);
+}
+
+titleScreen();
